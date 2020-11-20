16a06cb5b1b312c4b035f38fac1124ad07ddea29
Nina Kim <ninakim0504@hotmail.com>
2020-11-20 11:22:23 -0500

    setup heroku

--- react-notes-project-api/app.js
+++ react-notes-project-api/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const { MONGODB } = require('./config');
 const userRoutes = require('./routes/user');
@@ -11,6 +12,8 @@ const auth = require('./middleware/auth');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../react-notes-project/build')));
 
 app.use('/api/notes', auth, noteRoutes);
 app.use('/api/users', userRoutes);
@@ -29,7 +32,7 @@ app.use((err, req, res, next) => {
 mongoose.connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
         console.log('connected to mongodb');
-        return app.listen(3300);
+        return app.listen(process.env.PORT || 3300);
     })
     .then(() => console.log('server running at 3300'))
     .catch(err => console.log(err.message));
